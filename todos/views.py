f0f38d19e1285249ff1b218c00774dbe00bd5453
Juliana Marquez <mqjulis@gmail.com>
2024-08-27 21:54:00 -0300

    cabei

--- todos/views.py
+++ todos/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+
+
 def home(request):
-return HttpResponse('Olá mundo');
+    return render(request, "todos/home.html")
